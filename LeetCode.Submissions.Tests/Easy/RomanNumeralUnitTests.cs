fe0ca6134ecf28d14e5c0dbcbb29d9bd89827661
Jim Banning <jbanning1981@users.noreply.github.com>
2022-12-03 09:55:25 -0600

    Question 13: Add Roman numeral to integer conversion https://leetcode.com/problems/roman-to-integer/description/

---
+++ LeetCode.Submissions.Tests/Easy/RomanNumeralUnitTests.cs
@@ -0,0 +1,45 @@
+﻿using LeetCode.Submissions.Easy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Submissions.Tests.Easy
+{
+    [Trait("Category", "Easy")]
+    [Trait("Category", "Unit")]
+    public class RomanNumeralUnitTests
+    {
+        RomanNumeralService numeralService;
+
+        public RomanNumeralUnitTests()
+        {
+            numeralService = new RomanNumeralService();
+        }
+
+
+        [Theory]
+        [MemberData(nameof(TestNumerals))]
+        public void ConvertNumeral_WhenGivenNumeral_ConvertsToInt(string romanNumeral, int expectedValue)
+        {
+            var returnedValue = numeralService.ConvertNumeralToNumber(romanNumeral);
+
+            Assert.Equal(expectedValue, returnedValue);
+        }
+
+        public static IEnumerable<object[]> TestNumerals => new List<object[]>()
+        {
+            new object[] { "I", 1 },
+            new object[] { "II", 2 },
+            new object[] { "III", 3 },
+            new object[] { "IV", 4 },
+            new object[] { "IX", 9 },
+            new object[] { "M", 1000 },
+            new object[] { "MM", 2000 },
+            new object[] { "MCM", 1900 },
+            new object[] { "MCMXL", 1940 }
+        };
+        
+    }
+}
