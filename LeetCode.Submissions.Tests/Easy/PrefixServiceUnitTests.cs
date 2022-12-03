747b00b3950ea1e8bad292137ec36289a22ceed7
Jim B <jbanning1981@users.noreply.github.com>
2022-12-03 16:25:44 -0600

    Easy/q14 longest prefix (#4)
    * Initial Accepted Solution 1
    Runtime - 185ms; Beats 12.47%
    Memory - 38.6MB, beats 59.52%
    
    * Adding Version 2 and 3

---
+++ LeetCode.Submissions.Tests/Easy/PrefixServiceUnitTests.cs
@@ -0,0 +1,60 @@
+﻿using LeetCode.Submissions.Easy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Submissions.Tests.Easy
+{
+    [Trait("Category", "Easy")]
+    [Trait("Category", "Unit")]
+    public class PrefixServiceUnitTests
+    {
+        PrefixService prefixService;
+
+        public PrefixServiceUnitTests()
+        {
+            prefixService = new PrefixService();
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void LongestCommonPrefixVersionOne_WhenGivenNumeral_ConvertsToInt(string[] testValues, string expectedResult)
+        {
+            var returnedValue = prefixService.LongestCommonPrefixVersionOne(testValues);
+
+            Assert.Equal(expectedResult, returnedValue);
+        }
+
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void LongestCommonPrefixVersionTwo_WhenGivenNumeral_ConvertsToInt(string[] testValues, string expectedResult)
+        {
+            var returnedValue = prefixService.LongestCommonPrefixVersionTwo(testValues);
+
+            Assert.Equal(expectedResult, returnedValue);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void LongestCommonPrefixVersionThree_WhenGivenNumeral_ConvertsToInt(string[] testValues, string expectedResult)
+        {
+            var returnedValue = prefixService.LongestCommonPrefixVersionThree(testValues);
+
+            Assert.Equal(expectedResult, returnedValue);
+        }
+
+        public static IEnumerable<object[]> TestCases => new List<object[]>()
+        {
+            new object[] { new[] { "flower", "flow", "flight" }, "fl"  },
+            new object[] { new[] { "dog", "racecar", "car" }, ""  },
+            new object[] { new[] { "abcde"}, "abcde" },
+            new object[] { new[] { "", } , "" },
+            new object[] { new[] {"cir", "car" } ,"c" },
+            new object[] { new[] {"ab", "a" } ,"a" },
+            new object[] { new[] { "flower", "fkow" } ,"f" }
+        };
+    }
+}
---
+++ LeetCode.Submissions/Easy/PrefixService.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Submissions.Easy
+{
+    /*
+        Write a function to find the longest common prefix string amongst an array of strings.
+
+        If there is no common prefix, return an empty string "".
+
+        Example 1:
+
+            Input: strs = ["flower","flow","flight"]
+            Output: "fl"
+
+        Example 2:
+
+            Input: strs = ["dog","racecar","car"]
+            Output: ""
+            Explanation: There is no common prefix among the input strings.
+
+        Constraints:
+
+            1 <= strs.length <= 200
+            0 <= strs[i].length <= 200
+            strs[i] consists of only lowercase English letters.
+    */
+    public class PrefixService
+    {
+        private const int MaxLength = 200;
+
+        /// <summary>
+        /// Version One
+        /// Runtime - 185 ms Beats 12.47%
+        /// Memory - 38.6 MB Beats 59.52%
+        /// </summary>
+        /// <param name="textToCheck"></param>
+        /// <returns>The longest prefix in the array of strings</returns>
+        public string LongestCommonPrefixVersionOne(string[] textToCheck)
+        {
+            if (textToCheck.Length == 1)
+            {
+                return textToCheck[0];
+            }
+
+
+            var minLength = GetMinLength(textToCheck);
+
+            if (minLength == 0)
+            {
+                return "";
+            }
+            var prefixBuilder = new StringBuilder();
+            string longestPrefix = string.Empty;
+            bool isMatch = true;
+            for (int counter = 0; counter < minLength; counter++)
+            {
+                var compareLetter = textToCheck[0][counter];
+
+                foreach (var text in textToCheck)
+                {
+                    isMatch &= compareLetter == text[counter];
+                }
+
+                if (isMatch)
+                {
+                    prefixBuilder.Append(compareLetter);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (prefixBuilder.Length > longestPrefix.Length)
+            {
+                longestPrefix = prefixBuilder.ToString();
+            }
+
+            return longestPrefix;
+        }
+
+
+        /// <summary>
+        /// Version Two            
+        /// Runtime - 100 ms Beats 94.13 %
+        /// Memory - 40 MB Beats 19.16 %
+        /// </summary>
+        /// <param name="textToCheck"></param>
+        /// <returns>The longest prefix in the array of strings</returns>
+        public string LongestCommonPrefixVersionTwo(string[] textToCheck)
+        {
+
+            var arrayLength = textToCheck.Length;
+            if (arrayLength == 1) 
+            { 
+                return textToCheck[0];
+            }
+
+
+            var minLength = GetMinLength(textToCheck);
+
+            if(minLength == 0)
+            {
+                return "";
+            }
+            var prefixBuilder = new StringBuilder();
+            string longestPrefix = string.Empty;
+            bool isMatch = true;
+            for (int counter = 0; counter < minLength; counter++)
+            {
+                var compareLetter = textToCheck[0][counter];
+
+                for(int arrayCounter = 1; arrayCounter < arrayLength; arrayCounter++)
+                {
+                    isMatch &= compareLetter == textToCheck[arrayCounter][counter];
+                }
+
+                if(isMatch)
+                {
+                    prefixBuilder.Append(compareLetter);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if(prefixBuilder.Length > longestPrefix.Length) 
+            { 
+                return prefixBuilder.ToString();
+            }
+
+            return longestPrefix;
+        }
+
+        /// <summary>
+        /// Version Two            
+        /// Runtime- 157 ms Beats 58.50 %
+        /// Memory - 38.5 MB Beats 66.81 %
+        /// </summary>
+        /// <param name="textToCheck"></param>
+        /// <returns>The longest prefix in the array of strings</returns>
+        public string LongestCommonPrefixVersionThree(string[] textToCheck)
+        {
+            var arrayLength = textToCheck.Length;
+            if (arrayLength == 1)
+            {
+                return textToCheck[0];
+            }
+
+
+            var minLength = GetMinLength(textToCheck);
+
+            if (minLength == 0)
+            {
+                return "";
+            }
+            var prefixBuilder = new StringBuilder("");
+            bool isMatch = true;
+            for (int counter = 0; counter < minLength; counter++)
+            {
+                var compareLetter = textToCheck[0][counter];
+
+                for (int arrayCounter = 1; arrayCounter < arrayLength; arrayCounter++)
+                {
+                    isMatch &= compareLetter == textToCheck[arrayCounter][counter];
+                }
+
+                if (isMatch)
+                {
+                    prefixBuilder.Append(compareLetter);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return prefixBuilder.ToString();
+        }
+
+        private int GetMinLength(string[] textToCheck) 
+        { 
+            int maxLength = MaxLength;
+
+            foreach(string s in textToCheck) 
+            { 
+                maxLength = Math.Min(maxLength, s.Length);
+            }
+
+            return maxLength;
+        }
+
+    }
+}
