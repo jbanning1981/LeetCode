aea013e3c51149cc7358187898cc7fb565f05d47
Jim B <jbanning1981@users.noreply.github.com>
2022-12-03 19:33:17 -0600

    Question 20 - Valid Parenthesis (#5)
    1. URL: https://leetcode.com/problems/valid-parentheses/ Submission selected on 2nd attempt.
    2. Fixed naming issue with PrefixServiceUnitTests

---
+++ LeetCode.Submissions/Easy/ParenthesisService.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Submissions.Easy
+{
+    public class ParenthesisService
+    {
+        /// <summary>
+        /// Runtime - 98 ms Beats 77.42%
+        /// Memory - 37.2 MB Beats 29.25%
+        /// </summary>
+        /// <param name="textToValidate"></param>
+        /// <returns></returns>
+        public bool IsValid(string textToValidate)
+        {
+            if (string.IsNullOrWhiteSpace(textToValidate) || textToValidate.Length % 2 != 0) return false;
+
+            var bracketMap = new HashSet<(char open, char close)>()
+            {
+                ('(', ')'),
+                ('{', '}'),
+                ('[', ']'),
+            };
+
+
+            int pairCounter = 0;
+            char[] openBraces = new char[textToValidate.Length / 2];
+            for (int counter = 0; counter < textToValidate.Length; counter++)
+            {
+                if (counter == 0 && bracketMap.Any(key => key.close == textToValidate[0]))
+                {
+                    return false;
+                }
+
+                char c = textToValidate[counter];
+                if (bracketMap.Any(key => key.open == textToValidate[counter]))
+                {
+                    if (pairCounter >= textToValidate.Length / 2)
+                    {
+                        return false;
+                    }
+
+                    openBraces[pairCounter] = textToValidate[counter];
+                    pairCounter++;
+                    continue;
+                }
+                else
+                {
+                    if (pairCounter == 0) 
+                    { 
+                        return false; 
+                    }
+
+
+                    if (!bracketMap.Any(key => key.open == openBraces[(pairCounter - 1)] && key.close == textToValidate[counter]))
+                    {
+                        return false;
+                    }
+                    pairCounter--;
+                }
+
+
+
+            }
+
+
+            return pairCounter == 0;
+        }
+    }
+}
