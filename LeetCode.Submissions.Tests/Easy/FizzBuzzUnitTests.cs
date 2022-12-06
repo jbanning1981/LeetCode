ac1f2c8ac686da8bca5aeed491eaff908279a46a
Jim Banning <jbanning1981@users.noreply.github.com>
2022-12-06 02:05:25 -0600

    FizzBuzz solutions https://leetcode.com/problems/fizz-buzz/description/

---
+++ LeetCode.Submissions.Tests/Easy/FizzBuzzUnitTests.cs
@@ -0,0 +1,100 @@
+﻿using LeetCode.Submissions.Easy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Submissions.Tests.Easy
+{
+    [Trait("Category", "Unit")]
+    [Trait("Category", "Easy")]
+    public class FizzBuzzUnitTests
+    {
+        private FizzBuzzService fizzBuzzService;
+
+        public FizzBuzzUnitTests()
+        {
+            fizzBuzzService = new FizzBuzzService();
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void FizzBuzzV1_WhenProvidedValidInt_GeneratesListOutput(int arraySize, string[] expectedResult)
+        {
+            var returnedArray = fizzBuzzService.FizzBuzzV1(arraySize);
+
+            for(int counter = 0; counter < returnedArray.Count; counter++) 
+            {
+                Assert.Equal(expectedResult[counter], returnedArray[counter]);
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void FizzBuzzV2_WhenProvidedValidInt_GeneratesListOutput(int arraySize, string[] expectedResult)
+        {
+            var returnedArray = fizzBuzzService.FizzBuzzV2(arraySize);
+
+            for (int counter = 0; counter < returnedArray.Count; counter++)
+            {
+                Assert.Equal(expectedResult[counter], returnedArray[counter]);
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void FizzBuzzV3_WhenProvidedValidInt_GeneratesListOutput(int arraySize, string[] expectedResult)
+        {
+            var returnedArray = fizzBuzzService.FizzBuzzV3(arraySize);
+
+            for (int counter = 0; counter < returnedArray.Count; counter++)
+            {
+                Assert.Equal(expectedResult[counter], returnedArray[counter]);
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void FizzBuzzV4_WhenProvidedValidInt_GeneratesListOutput(int arraySize, string[] expectedResult)
+        {
+            var returnedArray = fizzBuzzService.FizzBuzzV4(arraySize);
+
+            for (int counter = 0; counter < returnedArray.Count; counter++)
+            {
+                Assert.Equal(expectedResult[counter], returnedArray[counter]);
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void FizzBuzzV5_WhenProvidedValidInt_GeneratesListOutput(int arraySize, string[] expectedResult)
+        {
+            var returnedArray = fizzBuzzService.FizzBuzzV5(arraySize);
+
+            for (int counter = 0; counter < returnedArray.Count; counter++)
+            {
+                Assert.Equal(expectedResult[counter], returnedArray[counter]);
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void FizzBuzzV6_WhenProvidedValidInt_GeneratesListOutput(int arraySize, string[] expectedResult)
+        {
+            var returnedArray = fizzBuzzService.FizzBuzzV6(arraySize);
+
+            for (int counter = 0; counter < returnedArray.Count; counter++)
+            {
+                Assert.Equal(expectedResult[counter], returnedArray[counter]);
+            }
+        }
+
+        public static IEnumerable<object[]> TestCases => new List<object[]>()
+        {
+            new object[] { 3, new[] { "1", "2", "Fizz" } },
+            new object[] { 5,  new[] { "1", "2", "Fizz", "4", "Buzz" }  },
+            new object[] { 15, new[] { "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz" } },
+        };
+    }
+}
