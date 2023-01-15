c227505a022f7a4c35bb07aae3b50273d075e836
Jim B <jbanning1981@users.noreply.github.com>
2023-01-14 22:57:30 -0600

    Equal Frequency P2433 (Easy) Runtime - 50.46% Memory - 37.61%

---
+++ LeetCode.Submissions/Easy/EqualFrequencyService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Submissions.Easy
+{
+    public class EqualFrequencyService
+    {
+        public bool EqualFrequency(string word)
+        {
+            if (word.Length == 2)
+            {
+                return true;
+            }
+
+            var letterCount = new Dictionary<char, int>();
+            foreach (char letter in word)
+            {
+                if (!letterCount.Any(kvp => kvp.Key == letter))
+                {
+                    letterCount.Add(letter, 1);
+                    continue;
+                }
+
+                letterCount[letter]++;
+            }
+
+            var min = letterCount.Min(kvp => kvp.Value);
+            var max = letterCount.Max(kvp => kvp.Value);
+            var maxMatchCount = letterCount.Count(kvp => kvp.Value == max);
+            var minMatchCount = letterCount.Count(kvp => kvp.Value == min);
+
+            return letterCount.Count() == 1 ||
+                    (letterCount.Count() == 2 && min == 1) ||
+                    (min == 1 && max == 1) ||
+                    (min == (max - 1) && maxMatchCount == 1) ||
+                    (min == 1 && minMatchCount == 1 && letterCount.All(kvp => kvp.Value == 1 || kvp.Value == max));
+        }
+    }
+}
