2a312087f38244536483e88bab9b07e46c499cfa
Jim B <jbanning1981@users.noreply.github.com>
2023-01-14 23:08:01 -0600

    Equal Frequency P2433 (Easy) (#7)
    Runtime - 50.46%
    Memory - 37.61%

---
+++ LeetCode.Submissions.Tests/Easy/EqualFrequencyServiceUnitTests.cs
@@ -0,0 +1,41 @@
+﻿using LeetCode.Submissions.Easy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LeetCode.Submissions.Tests.Easy
+{
+    [Trait("Category", "Unit")]
+    [Trait("Category", "Easy")]
+    public class EqualFrequencyServiceUnitTests
+    {
+        private EqualFrequencyService _equalService;
+
+        public EqualFrequencyServiceUnitTests()
+        {
+            _equalService = new EqualFrequencyService();
+        }
+
+
+        [Theory]
+        [InlineData("abcc",true)]
+        [InlineData("aazz", false)]
+        [InlineData("bac", true)]
+        [InlineData("ddaccb", false)]
+        [InlineData("abbcc", true)]
+        [InlineData("zz", true)]
+        [InlineData("cccd", true)]
+        [InlineData("aaaabbbbccc", false)]
+        [InlineData("babbdd", false)]
+        [InlineData("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz",true)]
+        public void EqualFrequencyServiceUnit(string wordToCheck, bool expectedResult)
+        {
+            var canEqualize = _equalService.EqualFrequency(wordToCheck);
+
+            Assert.Equal(expectedResult, canEqualize);
+        }
+    }
+}
