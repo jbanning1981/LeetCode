f052c59e5499bdfda990279f57e3e42653058e3b
Jim B <jbanning1981@users.noreply.github.com>
2024-04-21 01:11:35 -0500

    Figured out V2 (replaces array in place, without using a different array)

--- LeetCode.Submissions/Easy/RemoveElementService.cs
+++ LeetCode.Submissions/Easy/RemoveElementService.cs
@@ -24,10 +24,19 @@ for (int i = 0; i < actualLength; i++) {
 }
 
 
+What's not mentioned here is HOW the removed elements are replaced.
+The test cases are expecting you to replace elements a certain way
+Given input -> [0,1,2,2,3,0,4,2]
+Output -> [0,1,4,0,3]
+
+Note the 4 and 0 leapfrogging the 3 in order, so it's pulling non-matching elements in reverse order.
+
+
 */
 
 using System;
 using System.Diagnostics;
+using System.Security;
 
 public class RemoveElementService
 {
@@ -35,6 +44,9 @@ public class RemoveElementService
         return RemoveElementV2(nums, val);
     }
 
+    //Works - Accepted.
+    //Runtime - 124 ms (Beats 5.54%)
+    //Memory - 45.37MB (beats 95.38%)
     private int RemoveElementV1(int[] nums, int val)
     {
         //skipping null check, constraints guarantee a non-null array
@@ -47,7 +59,7 @@ public class RemoveElementService
         int[] filteredResults = new int[nums.Length];
         for(int counter = 0; counter < nums.Length; counter++)
         {
-
+            var debug = nums[counter];
             if(nums[counter] != val)
             {
                 filteredResults[filteredIndex] = nums[counter];
@@ -64,110 +76,43 @@ public class RemoveElementService
         return filteredIndex;
     }
 
-    private int RemoveElementFailed(int[] nums, int val)
-    {
-        //skipping null check, constraints guarantee a non-null array
-        if(nums.Length == 0)
-        {
-            return 0;
-        }
-
-        int replacementIndex = nums.Length;
-        int noMatchCount = 0;
-        for(int counter = 0; counter < nums.Length; counter++)
-        {
-            int debug = nums[counter];
-            if(nums[counter] != val)
-            {
-                noMatchCount++;
-
-
-                if(replacementIndex != nums.Length)
-                {
-                    nums[replacementIndex] = nums[counter];
-                    for(int nextReplacementIndex = replacementIndex+1; nextReplacementIndex < counter; nextReplacementIndex++)
-                    {
-                        if(nums[nextReplacementIndex] == val)
-                        {
-                            replacementIndex = nextReplacementIndex;
-                            break;
-                        }
-                    }
-                    continue;
-                }
-
-                replacementIndex = nums.Length;
-                continue;
-            }
-
-            if (replacementIndex == nums.Length)
-            {
-                replacementIndex = counter;                
-            }
-            
-
-                // {
-                //     nums[replacementIndex] = nums[counter];
-                //     //find the next replacementIndex
-                //     for(int i = 0; i<= counter; i++)
-                //     {
-                //         if(nums[i] == val)
-                //         {
-                //             replacementIndex = i;
-                //             break;
-                //         }
-                //     }
-                // }
-
-            // if(replacementIndex > counter)
-            // {
-            //     replacementIndex = counter;
-            // }
-        }
-
-        return noMatchCount;
-    }    
-    
+    //Works - Accepted.
+    //Runtime - 90 ms (Beats 91.70%)
+    //Memory - 46.02MB (beats 9.25%)
     private int RemoveElementV2(int[] nums, int val)
     {
         //skipping null check, constraints guarantee a non-null array
-        if(nums.Length == 0)
+        if (nums.Length == 0)
         {
             return 0;
         }
-
+        //var zz = string.Join(",", nums);
         int replacementIndex = nums.Length;
         int noMatchCount = 0;
         for (int counter = 0; counter < nums.Length; counter++)
         {
+
+            //If no match
             int debug = nums[counter];
             if (nums[counter] != val)
             {
                 noMatchCount++;
-                if (replacementIndex < counter)
-                {
-                    nums[replacementIndex] = nums[counter];                    
-                }
-                for (int i = replacementIndex; i < counter; i++)
-                {
-                    if (nums[i] == val)
-                    {
-                        replacementIndex = i;
-                        break;
-                    }
-                }
                 continue;
             }
 
-            if (replacementIndex >= counter)
+            for(int z = replacementIndex-1; z > counter; z--)
             {
-                replacementIndex = counter;                
+                if(nums[z] != val)
+                {
+                    nums[counter] = nums[z];
+                    replacementIndex = z;
+                    break;
+                }
             }
 
-
+            //zz = string.Join(",", nums);
         }
 
         return noMatchCount;
-    }        
-
+    }
 }
